be988a62937d195492e3a2faa05f508887e9c28e
Travis <tssund93@gmail.com>
2019-06-01 09:02:43 -0500

    init

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import           Lib
+
+import           Apecs
+import           Apecs.Gloss
+
+main :: IO ()
+main = do
+  w <- initWorld
+  runWith w $ do
+    initialize
+    play (InWindow "Shmup" (220, 360) (10, 10)) black 60 draw handleEvent step
